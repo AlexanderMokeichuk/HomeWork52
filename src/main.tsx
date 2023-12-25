56cc8e14795caa60d028234b658613234d27686c
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2023-12-25 22:28:08 +0600

    Initial commit!

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "../../../Software/Home work/HomeWork52/src/cards.css";
+import App from "../../../Software/Home work/HomeWork52/src/App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
